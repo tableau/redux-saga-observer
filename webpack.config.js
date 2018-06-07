e3340c42d94f9734f6220b52efd18979ee5dc4df
Rick Weber <rweber@tableau.com>
2018-06-07 11:30:32 -0700

    builds

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  entry: path.resolve(__dirname, 'src', 'Index.ts'),
 
   output: {
     path: path.resolve(__dirname, 'lib'),
