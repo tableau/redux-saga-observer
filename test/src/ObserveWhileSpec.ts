4be4e47b2bc6ea7c1b5e4143a02835e1782ffb35
Rick Weber <rweber@tableau.com>
2018-06-08 13:33:44 -0700

    test ObserveWhile

---
+++ test/src/ObserveWhileSpec.ts
@@ -0,0 +1,51 @@
+import { expectSaga } from 'redux-saga-test-plan';
+import { Action } from 'redux';
+
+import { observeWhile } from '../../src/effects/ObserveWhile';
+
+describe('ObserveWhile', () => {
+  type State = {
+    val: number;
+  }
+
+  interface MyAction extends Action {
+    val: number
+  };
+
+  it('should stop observing when condition becomes false', (done) => {
+    const initialState = {
+      val: 20
+    };
+
+    function* saga() {
+      yield observeWhile<State>(s => s.val < 40);
+    }
+
+    const reducer = (state: State, action: Action) => {
+      if (action.type === 'setVal') {
+        return {
+          ...state,
+          val: (action as MyAction).val
+        }
+      } else {
+        return state;
+      }
+    }
+
+    expectSaga(saga)
+      .withReducer(reducer, initialState)
+      .dispatch({ type: 'setVal', val: 35 })
+      .dispatch({ type: 'setVal', val: 45 })
+      .dispatch({ type: 'setVal', val: 50 })
+      .run(false)
+      .then(result => {
+        const state = result.storeState as State;
+
+        expect(state.val).toBe(45);
+      })
+      .catch(err => {
+        fail(err);
+      })
+      .then(_ => done());
+  });
+});
