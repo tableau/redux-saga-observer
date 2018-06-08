86dc4a4650a168a4b8d4c0d8db09443da173acaf
Rick Weber <rweber@tableau.com>
2018-06-08 13:32:31 -0700

    Test ObserveUntil

--- src/effects/ObserveUntil.ts
+++ src/effects/ObserveUntil.ts
@@ -1,17 +1,21 @@
-import { select, SelectEffect, take, TakeEffect } from 'redux-saga/effects';
+import { call, CallEffect, select, SelectEffect, take, TakeEffect } from 'redux-saga/effects';
 
 /**
  * A saga that returns when the passed invariant first becomes true. Returns immediately if it's true at time of call.
  * @param invariant The invariant on the redux state.
  */
-export function* observeUntil<S>(invariant: (state: S) => boolean): IterableIterator<TakeEffect<{}>> {
-  if (invariantMet(invariant)) {
+export function observeUntil<S>(invariant: (state: S) => boolean): CallEffect {
+  return call(observeUntilInternal, invariant);
+}
+
+function* observeUntilInternal<S>(invariant: (state: S) => boolean): IterableIterator<TakeEffect | CallEffect> {
+  if (yield call(invariantMet, invariant)) {
     return;
   }
 
   do {
     yield take('*');
-  } while (!invariantMet(invariant))
+  } while (!(yield call(invariantMet, invariant)));
 }
 
 function* invariantMet<S>(invariant: (state: S) => boolean): IterableIterator<SelectEffect | boolean> {
--- test/src/ObserveUntilSpec.ts
+++ test/src/ObserveUntilSpec.ts
@@ -1,7 +1,51 @@
+import { expectSaga } from 'redux-saga-test-plan';
+import { Action } from 'redux';
 
+import { observeUntil } from '../../src/effects/ObserveUntil';
 
-describe('Horse', () => {
-  it('', () => {
+describe('ObserveUntil', () => {
+  type State = {
+    val: number;
+  }
 
+  interface MyAction extends Action {
+    val: number
+  };
+
+  it('should stop observing when condition becomes true', (done) => {
+    const initialState = {
+      val: 20
+    };
+
+    function* saga() {
+      yield observeUntil<State>(s => s.val > 40);
+    }
+
+    const reducer = (state: State, action: Action) => {
+      if (action.type === 'setVal') {
+        return {
+          ...state,
+          val: (action as MyAction).val
+        }
+      } else {
+        return state;
+      }
+    }
+
+    expectSaga(saga)
+      .withReducer(reducer, initialState)
+      .dispatch({ type: 'setVal', val: 35 })
+      .dispatch({ type: 'setVal', val: 45 })
+      .dispatch({ type: 'setVal', val: 50 })
+      .run(false)
+      .then(result => {
+        const state = result.storeState as State;
+
+        expect(state.val).toBe(45);
+      })
+      .catch(err => {
+        fail(err);
+      })
+      .then(_ => done());
   });
 });
