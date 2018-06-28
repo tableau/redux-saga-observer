806c3bc4b24c29040dcfe806d59f54ec5a4ede74
Rick Weber <rweber@tableau.com>
2018-06-27 17:40:21 -0700

    Add a channel to ObserveUntil

--- src/effects/ObserveUntil.ts
+++ src/effects/ObserveUntil.ts
@@ -1,4 +1,13 @@
-import { call, CallEffect, select, SelectEffect, take, TakeEffect } from 'redux-saga/effects';
+import {
+  actionChannel,
+  ActionChannelEffect,
+  call,
+  CallEffect,
+  select,
+  SelectEffect,
+  take,
+  TakeEffect
+} from 'redux-saga/effects';
 
 /**
  * A saga that returns when the passed invariant first becomes true. Returns immediately if it's true at time of call.
@@ -8,18 +17,20 @@ export function observeUntil<S>(invariant: (state: S) => boolean): CallEffect {
   return call(observeUntilInternal, invariant);
 }
 
-function* observeUntilInternal<S>(invariant: (state: S) => boolean): IterableIterator<TakeEffect | CallEffect> {
+function* observeUntilInternal<S>(invariant: (state: S) => boolean): IterableIterator<ActionChannelEffect | TakeEffect | CallEffect> {
   if (yield call(invariantMet, invariant)) {
     return;
   }
 
+  const channel = yield actionChannel('*');
+
   do {
-    yield take('*');
+    yield take(channel);
   } while (!(yield call(invariantMet, invariant)));
 }
 
 function* invariantMet<S>(invariant: (state: S) => boolean): IterableIterator<SelectEffect | boolean> {
-  const state: S = yield select(state => state);
+  const state: S = yield select<S>(state => state);
 
   return invariant(state);
 }
